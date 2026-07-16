d731f9c76a86ba080b08bc591c1832bc810401fd
luiszy21 <luisfelipeportilho10@hotmail.com>
2026-07-16 10:19:05 -0300

    feat: adiciona lista de produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Produtos from "./pages/Produtos";
 function App() {
   return (
     <>
-      <div className="p-5 bg-cyan-950 text-gray-200 flex justify-center ">
-        <h1 className="text-2xl font-bold">Projeto BASE</h1>
+      <div className="p-5 bg-black text-white flex justify-center">
+        <h1 className="text-2xl font-bold">Projeto Base</h1>
       </div>
+      <Produtos />
     </>
   );
 }
