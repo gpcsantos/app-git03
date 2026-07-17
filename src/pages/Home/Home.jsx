214f28b1dddf8775105d80d58e8d17ff0ed81ecb
Ranielflemis <ranielsilv@gmail.com>
2026-07-17 09:13:32 -0300

    Gloria

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Bannerhero from "../../Components/Banner/Bannerhero.jsx";
+import Quemsomos from "../../components/Quemsomos/Quemsomos.jsx";
+
+function Home() {
+  return (
+    <>
+      <Bannerhero />
+      <Quemsomos />
+    </>
+  );
+}
+
+export default Home;
